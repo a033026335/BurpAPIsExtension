58488cc9bded1c52aae5add9f8637751b5125edc
Alex S <35503874+a033026335@users.noreply.github.com>
2024-08-25 22:47:03 -0500

    Update APIs2BurpRepeater_prod.py

--- APIs2BurpRepeater_prod.py
+++ APIs2BurpRepeater_prod.py
@@ -1,14 +1,14 @@
-#The purpose of this extension is to help pen testers to easily import Postman collections,
-#or cURL commands with all sizes from remediation lead and have it import directly into Burp Suite Repeater tab.
+#The purpose of this extension is to help pen testers to easily import http requests
+#with all sizes and have it import directly into Burp Suite Repeater tab.
 #This will help pen testers save a lot of time when there are large amount of API endpoints that are in scope,
 #by helping pen tester naming tab with approperiate endpoints, and to prevent sending individual call to the Repeater tab.
 
 from burp import IBurpExtender, ITab
-from javax.swing import JTabbedPane, ImageIcon, JFrame, JButton, JTextArea, JFileChooser, JPanel, JScrollPane, JLabel, BoxLayout
+from javax import swing
+from javax.swing import JTabbedPane, ImageIcon, JFrame, JButton, JTextArea, JFileChooser, JPanel, JScrollPane, JLabel, BoxLayout, SwingConstants
 from java.awt import Font, BorderLayout
 from java.net import URL
 from javax.swing import JCheckBox
-from javax.imageio import ImageIO
 
 class BurpExtender(IBurpExtender, ITab):
 
@@ -26,29 +26,44 @@ class BurpExtender(IBurpExtender, ITab):
         self.panel.setLayout(BoxLayout(self.panel, BoxLayout.Y_AXIS))
 
         # Welcome label at the top
-        welcomeLabel = JLabel("Welcome to APIs2BurpRepeater Extension")
+        welcomeLabel = JLabel("Welcome to APIs2BurpRepeater Extension", SwingConstants.LEFT)
         welcomeLabel.setFont(Font("SansSerif", Font.BOLD, 25))
-        self.panel.add(welcomeLabel, BorderLayout.NORTH)
+        self.panel.add(welcomeLabel, BorderLayout.PAGE_START)
 
         #Instructions on how to use the extension
-        instructionmessage = JLabel(">Please follow the following instruction to formate endopoint names, import http requests, combine datas then sent to Repeater Tab.")
+        instructionmessage = JLabel(">  This extension aids in adding the names to its corresponding APIs endpoints by parsing endpoint names with corrosponding http request of API endpoints in the Repeater Tab.")
         self.panel.add(instructionmessage, BorderLayout.CENTER)
-        instructionmessage1 = JLabel("1: You would want to process and formate your list of endpoint names for all of your HTTP requests.")
+        instructionmessage.setFont(Font("SansSerif",Font.PLAIN,14))
+        instructionmessage9 = JLabel("      Follow the instructions below to Format Endpoint Names, Import HTTP requests via .txt file, and combine both data sets to send to Repeater Tab. ")
+        self.panel.add(instructionmessage9, BorderLayout.CENTER)
+        instructionmessage9.setFont(Font("SansSerif",Font.PLAIN,14))
+        instructionmessage8 = JLabel("   ")
+        self.panel.add(instructionmessage8, BorderLayout.CENTER)
+        instructionmessage1 = JLabel("1: In the Top Text Area box, enter the endpoint names in a list format according to how they are listed in the file of endpoints.")
         self.panel.add(instructionmessage1, BorderLayout.CENTER)
-        instructionmessage1 = JLabel("You will go ahead and copy entire column of endpoint names from pen test qestionnaire, then paste in the top text box.")
-        self.panel.add(instructionmessage1, BorderLayout.CENTER)
-        instructionmessage1 = JLabel("If the solution contain external proxies, make sure to check the check box that says external APIs. Then click formate endpoint names.")
-        self.panel.add(instructionmessage1, BorderLayout.CENTER)
-        instructionmessage1 = JLabel("You then will have formated endpoint names in the order of the questionnaire at the bottom text box that will be utilize by the repeater tab.")
-        self.panel.add(instructionmessage1, BorderLayout.CENTER)
-        instructionmessage1 = JLabel("2, you will go ahead and click clear top text box button to clear pasted endpoint names, then click import http requests .txt file button.")
-        self.panel.add(instructionmessage1, BorderLayout.CENTER)
-        instructionmessage1 = JLabel("Select file that contains all http requests that are in the corresponding order with the formated endpoint names.")
-        self.panel.add(instructionmessage1, BorderLayout.CENTER)
-        instructionmessage1 = JLabel("3, you can just click send to repeater tab button. Then all http requests should have corresponding endpoint name in the order which it was imported.")
-        self.panel.add(instructionmessage1, BorderLayout.CENTER)
-        instructionmessage1 = JLabel("Note: Default protocol is http and port is 80. Make sure to switch to https protocol and port 443 port using the pencil icon in repeater tab.")
-        self.panel.add(instructionmessage1, BorderLayout.CENTER)
+        instructionmessage1.setFont(Font("SansSerif",Font.PLAIN,14))
+        instructionmessage3 = JLabel("     If there are external APIs, make sure to check the box that says, External APIs.  Then hit Format Endpoint Names.")
+        self.panel.add(instructionmessage3, BorderLayout.CENTER)
+        instructionmessage3.setFont(Font("SansSerif",Font.PLAIN,14))
+        instructionmessage.setFont(Font("SansSerif",Font.PLAIN,14))
+        instructionmessage8 = JLabel("   ")
+        self.panel.add(instructionmessage8, BorderLayout.CENTER)
+        instructionmessage5 = JLabel("2: Hit the Clear Top Text Area button.")
+        self.panel.add(instructionmessage5, BorderLayout.CENTER)
+        instructionmessage5.setFont(Font("SansSerif",Font.PLAIN,14))
+        instructionmessage8 = JLabel("   ")
+        self.panel.add(instructionmessage8, BorderLayout.CENTER)
+        instructionmessage6 = JLabel("3: Hit the Import HTTP requests .txt File button. Select the file that contains all HTTP requests in the same order as the endpoint names list from Step 1.")
+        self.panel.add(instructionmessage6, BorderLayout.CENTER)
+        instructionmessage6.setFont(Font("SansSerif",Font.PLAIN,14))
+        instructionmessage.setFont(Font("SansSerif",Font.PLAIN,14))
+        instructionmessage8 = JLabel("   ")
+        self.panel.add(instructionmessage8, BorderLayout.CENTER)
+        instructionmessage7 = JLabel("4: Hit the Send to Repeater Tab button.  All HTTP requests will be formatted according to the list and the file that was imported. ")
+        self.panel.add(instructionmessage7, BorderLayout.CENTER)
+        instructionmessage7.setFont(Font("SansSerif",Font.PLAIN,14))
+        instructionmessage8 = JLabel("   ")
+        self.panel.add(instructionmessage8, BorderLayout.CENTER)
 
         # Buttons at the top center
         buttonPanel = JPanel()
@@ -61,7 +76,7 @@ class BurpExtender(IBurpExtender, ITab):
         self.formatEndpointsButton = JButton("Format Endpoint Names", actionPerformed=self.formatEndpoints)
         self.chooseFileButton = JButton("Import HTTP requests .txt File", actionPerformed=self.chooseFile)
         self.toRepeaterButton = JButton("Send to Repeater Tab", actionPerformed=self.sendToRepeater)
-        self.clearButton = JButton("Clear Top Text Box", actionPerformed=self.clearText)
+        self.clearButton = JButton("Clear Top Text Area", actionPerformed=self.clearText)
         buttonPanel.add(self.formatEndpointsButton)
         buttonPanel.add(self.chooseFileButton)
         buttonPanel.add(self.toRepeaterButton)
@@ -71,11 +86,15 @@ class BurpExtender(IBurpExtender, ITab):
         # Text area for displaying imported requests named "textArea"
         self.textArea = JTextArea(10, 50)
         self.scrollPane = JScrollPane(self.textArea)
-        self.panel.add(self.scrollPane, BorderLayout.CENTER)
+        toptextArea = swing.JLabel("Top Text Area:")
+        self.panel.add(toptextArea,  BorderLayout.CENTER)
+        self.panel.add(self.scrollPane,  BorderLayout.CENTER)
 
         # Second text area for displaying formatted endpoints named "endpointsTextArea"
         self.endpointsTextArea = JTextArea(10, 50)
         self.endpointsScrollPane = JScrollPane(self.endpointsTextArea)
+        bottomtextArea = swing.JLabel("Bottom Text Area:")
+        self.panel.add(bottomtextArea, BorderLayout.CENTER)
         self.panel.add(self.endpointsScrollPane, BorderLayout.CENTER)
         self.frame.getContentPane().add(self.panel)
         
@@ -138,7 +157,7 @@ class BurpExtender(IBurpExtender, ITab):
         currentRequest = []
         endpointIndex = 0  # Index to track the current endpoint
         for line in lines:
-            if line.split(" ")[0].upper() in ["GET", "POST", "PUT", "DELETE", "PATCH", "OPTION", "HEAD"]:
+            if line.split(" ")[0].upper() in ["GET", "POST", "PUT", "DELETE", "PATCH", "OPTION", "HEAD", "UPDATE"]:
                 if currentRequest:
                     requests.append("\n".join(currentRequest))
                     currentRequest = []
